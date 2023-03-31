3d2f12424a863f406a19e3ba0220a895f7da9a68
Hasnathasan <hasnatoooooooo@gmail.com>
2023-03-31 15:09:46 +0600

    Main section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,14 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="header"> 
-            <h2>Programming Cafe</h2>
-            <img src={userImg} alt="" />
+        <div>
+            <div className="header"> 
+                <h2>Programming Cafe</h2>
+                <img src={userImg} alt="" />
+            </div>
+            <hr />
         </div>
+        
     );
 };
 
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div className="main-section">
+            <div className="blogs">
+                <h1>Blogs</h1>
+            </div>
+            <div className="bookmarks">
+                <h1>Books</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
